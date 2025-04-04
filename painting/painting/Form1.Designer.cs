2d0eab3c97154a3aec3fda5a2fcb696185ed3851
Fecinius <vranekmatyas07@gmail.com>
2025-04-04 09:32:10 +0200

    Add files via upload

--- painting/painting/Form1.Designer.cs
+++ painting/painting/Form1.Designer.cs
@@ -29,36 +29,38 @@
         private void InitializeComponent()
         {
             this.panelDraw = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBoxWhite = new System.Windows.Forms.PictureBox();
-            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
-            this.pictureBoxOrange = new System.Windows.Forms.PictureBox();
-            this.pictureBoxYellow = new System.Windows.Forms.PictureBox();
-            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
-            this.pictureBoxCayn = new System.Windows.Forms.PictureBox();
-            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
-            this.pictureBoxPink = new System.Windows.Forms.PictureBox();
-            this.pictureBoxBlack = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.buttonClasicPen = new System.Windows.Forms.Button();
             this.buttonSpray = new System.Windows.Forms.Button();
+            this.buttonClasicPen = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxBlack = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPink = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCayn = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+            this.pictureBoxYellow = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOrange = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxWhite = new System.Windows.Forms.PictureBox();
+            this.textBoxPenSize = new System.Windows.Forms.TextBox();
             this.panelDraw.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWhite)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCayn)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPink)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlack)).BeginInit();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPink)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCayn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWhite)).BeginInit();
             this.SuspendLayout();
             // 
             // panelDraw
             // 
             this.panelDraw.BackColor = System.Drawing.Color.White;
+            this.panelDraw.Controls.Add(this.textBoxPenSize);
             this.panelDraw.Controls.Add(this.panel2);
             this.panelDraw.Controls.Add(this.panel1);
             this.panelDraw.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -69,6 +71,36 @@
             this.panelDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelDraw_MouseDown);
             this.panelDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelDraw_MouseMove);
             this.panelDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelDraw_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.buttonSpray);
+            this.panel2.Controls.Add(this.buttonClasicPen);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(438, 36);
+            this.panel2.TabIndex = 1;
+            // 
+            // buttonSpray
+            // 
+            this.buttonSpray.Location = new System.Drawing.Point(84, 3);
+            this.buttonSpray.Name = "buttonSpray";
+            this.buttonSpray.Size = new System.Drawing.Size(75, 30);
+            this.buttonSpray.TabIndex = 10;
+            this.buttonSpray.Text = "SPRAY";
+            this.buttonSpray.UseVisualStyleBackColor = true;
+            this.buttonSpray.Click += new System.EventHandler(this.buttonSpray_Click);
+            // 
+            // buttonClasicPen
+            // 
+            this.buttonClasicPen.Location = new System.Drawing.Point(3, 3);
+            this.buttonClasicPen.Name = "buttonClasicPen";
+            this.buttonClasicPen.Size = new System.Drawing.Size(75, 30);
+            this.buttonClasicPen.TabIndex = 9;
+            this.buttonClasicPen.Text = "PEN";
+            this.buttonClasicPen.UseVisualStyleBackColor = true;
+            this.buttonClasicPen.Click += new System.EventHandler(this.buttonClasicPen_Click);
             // 
             // panel1
             // 
@@ -87,75 +119,15 @@
             this.panel1.Size = new System.Drawing.Size(328, 36);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBoxWhite
+            // pictureBoxBlack
             // 
-            this.pictureBoxWhite.BackColor = System.Drawing.Color.White;
-            this.pictureBoxWhite.Location = new System.Drawing.Point(3, 3);
-            this.pictureBoxWhite.Name = "pictureBoxWhite";
-            this.pictureBoxWhite.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxWhite.TabIndex = 0;
-            this.pictureBoxWhite.TabStop = false;
-            this.pictureBoxWhite.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxRed
-            // 
-            this.pictureBoxRed.BackColor = System.Drawing.Color.Red;
-            this.pictureBoxRed.Location = new System.Drawing.Point(39, 3);
-            this.pictureBoxRed.Name = "pictureBoxRed";
-            this.pictureBoxRed.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxRed.TabIndex = 1;
-            this.pictureBoxRed.TabStop = false;
-            this.pictureBoxRed.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxOrange
-            // 
-            this.pictureBoxOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.pictureBoxOrange.Location = new System.Drawing.Point(75, 3);
-            this.pictureBoxOrange.Name = "pictureBoxOrange";
-            this.pictureBoxOrange.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxOrange.TabIndex = 2;
-            this.pictureBoxOrange.TabStop = false;
-            this.pictureBoxOrange.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxYellow
-            // 
-            this.pictureBoxYellow.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBoxYellow.Location = new System.Drawing.Point(111, 3);
-            this.pictureBoxYellow.Name = "pictureBoxYellow";
-            this.pictureBoxYellow.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxYellow.TabIndex = 3;
-            this.pictureBoxYellow.TabStop = false;
-            this.pictureBoxYellow.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxGreen
-            // 
-            this.pictureBoxGreen.BackColor = System.Drawing.Color.Lime;
-            this.pictureBoxGreen.Location = new System.Drawing.Point(147, 3);
-            this.pictureBoxGreen.Name = "pictureBoxGreen";
-            this.pictureBoxGreen.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxGreen.TabIndex = 4;
-            this.pictureBoxGreen.TabStop = false;
-            this.pictureBoxGreen.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxCayn
-            // 
-            this.pictureBoxCayn.BackColor = System.Drawing.Color.Cyan;
-            this.pictureBoxCayn.Location = new System.Drawing.Point(185, 3);
-            this.pictureBoxCayn.Name = "pictureBoxCayn";
-            this.pictureBoxCayn.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxCayn.TabIndex = 5;
-            this.pictureBoxCayn.TabStop = false;
-            this.pictureBoxCayn.Click += new System.EventHandler(this.pictureBoxBlack_Click);
-            // 
-            // pictureBoxBlue
-            // 
-            this.pictureBoxBlue.BackColor = System.Drawing.Color.Blue;
-            this.pictureBoxBlue.Location = new System.Drawing.Point(221, 3);
-            this.pictureBoxBlue.Name = "pictureBoxBlue";
-            this.pictureBoxBlue.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxBlue.TabIndex = 6;
-            this.pictureBoxBlue.TabStop = false;
-            this.pictureBoxBlue.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            this.pictureBoxBlack.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxBlack.Location = new System.Drawing.Point(293, 3);
+            this.pictureBoxBlack.Name = "pictureBoxBlack";
+            this.pictureBoxBlack.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxBlack.TabIndex = 8;
+            this.pictureBoxBlack.TabStop = false;
+            this.pictureBoxBlack.Click += new System.EventHandler(this.pictureBoxBlack_Click);
             // 
             // pictureBoxPink
             // 
@@ -167,43 +139,84 @@
             this.pictureBoxPink.TabStop = false;
             this.pictureBoxPink.Click += new System.EventHandler(this.pictureBoxBlack_Click);
             // 
-            // pictureBoxBlack
+            // pictureBoxBlue
             // 
-            this.pictureBoxBlack.BackColor = System.Drawing.Color.Black;
-            this.pictureBoxBlack.Location = new System.Drawing.Point(293, 3);
-            this.pictureBoxBlack.Name = "pictureBoxBlack";
-            this.pictureBoxBlack.Size = new System.Drawing.Size(30, 30);
-            this.pictureBoxBlack.TabIndex = 8;
-            this.pictureBoxBlack.TabStop = false;
-            this.pictureBoxBlack.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            this.pictureBoxBlue.BackColor = System.Drawing.Color.Blue;
+            this.pictureBoxBlue.Location = new System.Drawing.Point(221, 3);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxBlue.TabIndex = 6;
+            this.pictureBoxBlue.TabStop = false;
+            this.pictureBoxBlue.Click += new System.EventHandler(this.pictureBoxBlack_Click);
             // 
-            // panel2
+            // pictureBoxCayn
             // 
-            this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Controls.Add(this.buttonSpray);
-            this.panel2.Controls.Add(this.buttonClasicPen);
-            this.panel2.Location = new System.Drawing.Point(12, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(438, 36);
-            this.panel2.TabIndex = 1;
+            this.pictureBoxCayn.BackColor = System.Drawing.Color.Cyan;
+            this.pictureBoxCayn.Location = new System.Drawing.Point(185, 3);
+            this.pictureBoxCayn.Name = "pictureBoxCayn";
+            this.pictureBoxCayn.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxCayn.TabIndex = 5;
+            this.pictureBoxCayn.TabStop = false;
+            this.pictureBoxCayn.Click += new System.EventHandler(this.pictureBoxBlack_Click);
             // 
-            // buttonClasicPen
+            // pictureBoxGreen
             // 
-            this.buttonClasicPen.Location = new System.Drawing.Point(3, 3);
-            this.buttonClasicPen.Name = "buttonClasicPen";
-            this.buttonClasicPen.Size = new System.Drawing.Size(75, 30);
-            this.buttonClasicPen.TabIndex = 9;
-            this.buttonClasicPen.Text = "PEN";
-            this.buttonClasicPen.UseVisualStyleBackColor = true;
+            this.pictureBoxGreen.BackColor = System.Drawing.Color.Lime;
+            this.pictureBoxGreen.Location = new System.Drawing.Point(147, 3);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxGreen.TabIndex = 4;
+            this.pictureBoxGreen.TabStop = false;
+            this.pictureBoxGreen.Click += new System.EventHandler(this.pictureBoxBlack_Click);
             // 
-            // buttonSpray
+            // pictureBoxYellow
             // 
-            this.buttonSpray.Location = new System.Drawing.Point(84, 3);
-            this.buttonSpray.Name = "buttonSpray";
-            this.buttonSpray.Size = new System.Drawing.Size(75, 30);
-            this.buttonSpray.TabIndex = 10;
-            this.buttonSpray.Text = "SPRAY";
-            this.buttonSpray.UseVisualStyleBackColor = true;
+            this.pictureBoxYellow.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBoxYellow.Location = new System.Drawing.Point(111, 3);
+            this.pictureBoxYellow.Name = "pictureBoxYellow";
+            this.pictureBoxYellow.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxYellow.TabIndex = 3;
+            this.pictureBoxYellow.TabStop = false;
+            this.pictureBoxYellow.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            // 
+            // pictureBoxOrange
+            // 
+            this.pictureBoxOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBoxOrange.Location = new System.Drawing.Point(75, 3);
+            this.pictureBoxOrange.Name = "pictureBoxOrange";
+            this.pictureBoxOrange.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxOrange.TabIndex = 2;
+            this.pictureBoxOrange.TabStop = false;
+            this.pictureBoxOrange.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.BackColor = System.Drawing.Color.Red;
+            this.pictureBoxRed.Location = new System.Drawing.Point(39, 3);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxRed.TabIndex = 1;
+            this.pictureBoxRed.TabStop = false;
+            this.pictureBoxRed.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            // 
+            // pictureBoxWhite
+            // 
+            this.pictureBoxWhite.BackColor = System.Drawing.Color.White;
+            this.pictureBoxWhite.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxWhite.Name = "pictureBoxWhite";
+            this.pictureBoxWhite.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxWhite.TabIndex = 0;
+            this.pictureBoxWhite.TabStop = false;
+            this.pictureBoxWhite.Click += new System.EventHandler(this.pictureBoxBlack_Click);
+            // 
+            // textBoxPenSize
+            // 
+            this.textBoxPenSize.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxPenSize.Location = new System.Drawing.Point(456, 21);
+            this.textBoxPenSize.Name = "textBoxPenSize";
+            this.textBoxPenSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPenSize.TabIndex = 11;
+            this.textBoxPenSize.TextChanged += new System.EventHandler(this.textBoxPenSize_TextChanged);
             // 
             // Form1
             // 
@@ -214,17 +227,18 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.panelDraw.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWhite)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCayn)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPink)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlack)).EndInit();
+            this.panelDraw.PerformLayout();
             this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPink)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCayn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWhite)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -245,6 +259,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button buttonSpray;
         private System.Windows.Forms.Button buttonClasicPen;
+        private System.Windows.Forms.TextBox textBoxPenSize;
     }
 }
 
--- painting/painting/Form1.cs
+++ painting/painting/Form1.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace painting
 {
@@ -20,13 +21,13 @@ namespace painting
         int mouseY;
         int penswitcher;
         int penSize;
-        
+
         public Form1()
         {
-           
+
             InitializeComponent();
             g = panelDraw.CreateGraphics();
-            penSize = 8;
+            penSize = 6;
             cursorPen = new Pen(Color.Black, penSize);
             penswitcher = 0;
             if (penswitcher == 0)
@@ -37,19 +38,16 @@ namespace painting
             }
             else if (penswitcher == 1) 
             {
-                int radius = penSize;
-                float dotSizes = penSize / 8;
-
 
             }
- 
+
         }
 
         private void pictureBoxBlack_Click(object sender, EventArgs e)
         {
             PictureBox color = (PictureBox)sender;
             cursorPen.Color = color.BackColor;
-           
+
         }
 
         private void panelDraw_MouseDown(object sender, MouseEventArgs e)
@@ -68,21 +66,62 @@ namespace painting
 
         private void panelDraw_MouseMove(object sender, MouseEventArgs e)
         {
-            if (mouseMoving && mouseX != -1 && mouseY != -1) 
-            { 
-                if(penswitcher == 0)
+            if (mouseMoving && mouseX != -1 && mouseY != -1)
+            {
+                if (penswitcher == 0)
                 {
+                    cursorPen.Width = penSize;
                     g.DrawLine(cursorPen, new Point(mouseX, mouseY), e.Location);
                     mouseX = e.X;
                     mouseY = e.Y;
                 }
                 else if (penswitcher == 1)
                 {
-                    
+
                     Random rng = new Random();
-                    g.DrawLine()
+                    int radius = penSize;
+                    cursorPen.Width = 2;
+
+                    for (int i = 0; i < rng.Next(radius/2,radius*2); i++)
+                    {
+                        int sprayX = mouseX + rng.Next(-radius, radius + 1);
+                        int sprayY = mouseY + rng.Next(-radius, radius + 1);
+                        g.DrawLine(cursorPen, new Point(sprayX, sprayY), new Point(sprayX + 1, sprayY + 1)); 
+                    }                   
+                    mouseX = e.X;
+                    mouseY = e.Y;
                 }
-               
+
+            }
+        }
+
+        private void buttonClasicPen_Click(object sender, EventArgs e)
+        {
+            penswitcher = 0;
+        }
+
+        private void buttonSpray_Click(object sender, EventArgs e)
+        {
+            penswitcher = 1;
+            if (penSize<8)
+            {
+                textBoxPenSize.Text = "8";
+            }
+        }
+
+        private void textBoxPenSize_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxPenSize.Text.Length == 0) return; // Prevents error on empty text
+
+            if (int.TryParse(textBoxPenSize.Text, out int penSize) && penSize > 0 && penSize.ToString().Length<3 )
+            {
+                this.penSize = penSize;
+            }
+            else
+            {
+                //textBoxPenSize.Text = textBoxPenSize.Text.Remove(textBoxPenSize.Text.Length - 1);
+                textBoxPenSize.Text = textBoxPenSize.Text.Substring(0, textBoxPenSize.Text.Length - 1);
+                textBoxPenSize.SelectionStart = textBoxPenSize.Text.Length;
             }
         }
     }
